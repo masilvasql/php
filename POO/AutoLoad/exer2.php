a35caf7d3dfac432d88e3814d00ad19bc4cd0ba5
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-03-28 19:13:40 -0300

    autload

--- POO/AutoLoad/exer2.php
+++ POO/AutoLoad/exer2.php
@@ -2,14 +2,16 @@
 
     function incluirClasses($nomeClasse){ // VERIFICA SE A CLASSE CHAMADA EXISTE DENTRO DA PASTA ATUAL
         if(file_exists($nomeClasse.".php") === true){
+            echo var_dump($nomeClasse);
             require_once($nomeClasse.".php");
         }
     }
     
     spl_autoload_register("incluirClasses"); //standard php library = spl // VAI CHAMAR A FUNÇÃO incluirClasses (é passada como string mesmo)
 
-    spl_autoload_register(function($nomeClasse){ // VAI VERIFICAR SE EXISTE A CLASSE NA PASTA CHAMADA A BAIXO
+    spl_autoload_register(function($nomeClasse){ // VAI VERIFICAR SE EXISTE A CLASSE ABSTRATA NA PASTA CHAMADA A BAIXO
         if(file_exists("abstratas" . DIRECTORY_SEPARATOR . $nomeClasse.".php") === true){
+            echo var_dump($nomeClasse);
             require_once('abstratas'. DIRECTORY_SEPARATOR . $nomeClasse.".php");
         }
     });
